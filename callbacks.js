215c3050e046a4e6045d0b6babce6f230349784c
Chloe Janko <chloenoelle98@gmail.com>
2017-06-16 11:11:33 -0700

    completed assignment8

--- callbacks.js
+++ callbacks.js
@@ -18,11 +18,17 @@
 
 // Write a function called firstItem that passes the first item of the given array to the callback function
 
+
 var foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
 firstItem(foods, (firstItem) => {
   console.log('The first item is ' + firstItem);
 });
+
+function firstItem = (myArr, cb){
+  cb(myArr[0]);
+};
+
 
 // Write a function called getLength that passes the length of the array into the callback
 
@@ -30,11 +36,19 @@ getLength(foods, (length) => {
   console.log('The length of the array is ' + length);
 });
 
+function getLength = (myArr, cb){
+	cb(myArr.length);
+};
+
 // Write a function called last which passes the last item of the array into the callback
 
 last(foods, (lastItem) => {
   console.log('The last item in the array is ' + lastItem);
 });
+
+function last = (myArr, cb){
+	cb(myArr[myArr.length -1]);
+};
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
 
@@ -42,11 +56,19 @@ sumNums(5, 10, (sum) => {
   console.log('The sum is ' + sum);
 });
 
-// Write a function called multiplyNums that adds two numbers and passes the result to the callback
+function sumNums = (n1, n2, cb){
+	cb(n1 + n2);
+};
+
+// Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
 
 multiplyNums(5, 10, (product) => {
   console.log('The product is ' + product);
 });
+
+function multiplyNums = (n1, n2, cb){
+	cb(n1 * n2);
+}
 
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
@@ -55,6 +77,16 @@ contains(foods, 'ribeye', (result) => {
   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
 });
 
+function contains = (myArr, item, cb){
+	var check = false;
+	for(var i = 0; i < myArr.length; i++){
+		if(myArr[i] === item){
+			check = true;
+		}
+	}
+	cb(check);
+}
+
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 
@@ -62,8 +94,62 @@ removeDuplicates(foods, (uniqueFoods) => {
   console.log('foods with duplicates removed: ' + uniqueFoods);
 });
 
+function removeDuplicates = (myArr, cb){
+
+//indexOf returns the index of the first occurrence of searchElement in the array, 
+//or -1 if searchElement is not found.
+//View more examples on the bottom of this file.
+
+var selectFoods = [];
+	for(var i = 0; i < myArr.length; i++){
+		if(selectFoods.indexOf(myArr[i]) == -1){
+			//pushes foods with only one occurence to selectFoods
+			selectFoods.push(myArr[i]);
+		}
+	}
+	cb(selectFoods);
+}
+
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
 
 forEach(foods, (value, index) => {
   console.log('${value} is at index ' + index);
 });
+
+function forEach = (){
+	for(var i = 0; i< myArr.length; i++){
+		var val = myArr[i];
+		var index = i;
+		cb(val,index);
+	}
+}
+
+
+/*
+
+indexOf EXAMPLES
+
+// Create an array. (The elements start at index 0.)  
+var ar = ["ab", "cd", "ef", "ab", "cd"];  
+
+// Determine the first location of "cd".  
+document.write(ar.indexOf("cd") + "<br/>");  
+
+// Output: 1  
+
+// Find "cd" starting at index 2.  
+document.write(ar.indexOf("cd", 2) + "<br/>");  
+
+// Output: 4  
+
+// Find "gh" (which is not found).  
+document.write (ar.indexOf("gh")+ "<br/>");  
+
+// Output: -1  
+
+// Find "ab" with a fromIndex argument of -2.  
+// The search starts at index 3, which is the array length plus -2.  
+document.write (ar.indexOf("ab", -2) + "<br/>");  
+// Output: 3  
+
+*/
